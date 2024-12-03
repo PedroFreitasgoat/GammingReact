7f32dd9fe0dc20d6dde51af9478f59707cc9f27d
PdroFreitass <pedrothdvzs@gmail.com>
2024-12-02 23:38:54 -0300

    Inicio do projeto Gamming com React e Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import TrandingGames from './components/TrendingGames/TrandingGames'
+
+const App = () => {
+  return (
+      <>
+      <Navbar />
+      <Hero />
+      <TrandingGames/>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/TrendingGames/TrandingGames.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrandingGames = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TrandingGames
